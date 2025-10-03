62daa6981a43b672c5782e860977530108f42665
Willoh Brubaker <willoh_brubaker@outlook.com>
2025-10-02 21:55:28 -0500

    Fix for helb.c and hello.c functions

--- gelb/gelb.c
+++ gelb/gelb.c
@@ -3,33 +3,35 @@
 
 void gelb(void)
 {
-    static char gelb_code[] = "\033[93m";
+    char gelb_code[] = "\033[93m";
     static char reset_code[] = "\033[0m";
     static long gelb_len = 5;
     static long reset_len = 4;
     long ret;
 
     // Write gelb color code
+
     asm volatile(
-        "mov $1, %%rax\n"
-        "mov $1, %%rdi\n"
-        "mov %1, %%rsi\n"
-        "mov $6, %%rdx\n"
-        "syscall\n"
-        : "=a"(ret)
-        : "r"(gelb_code), "r"(gelb_len)
-        : "rcx", "rdi", "rsi", "rdx", "r11", "memory");
+        "mov $1, %%rax;" // System call number for write (1)
+        "mov $1, %%rdi;" // File descriptor 1 (stdout)
+        "mov %0, %%rsi;" // Address of the message
+        "mov %1, %%rdx;" // Length of the message
+        "syscall;"       // Invoke the system call
+        :
+        : "r"(gelb_code), "r"((long)gelb_len)
+        : "%rax", "%rdi", "%rsi", "%rdx");
 
     hello();
 
     // Write reset color code
+
     asm volatile(
-        "mov $1, %%rax\n"
-        "mov $1, %%rdi\n"
-        "mov %1, %%rsi\n"
-        "mov $5, %%rdx\n"
-        "syscall\n"
-        : "=a"(ret)
-        : "r"(reset_code), "r"(reset_len)
-        : "rcx", "rdi", "rsi", "rdx", "r11", "memory");
+        "mov $1, %%rax;" // System call number for write (1)
+        "mov $1, %%rdi;" // File descriptor 1 (stdout)
+        "mov %0, %%rsi;" // Address of the message
+        "mov %1, %%rdx;" // Length of the message
+        "syscall;"       // Invoke the system call
+        :
+        : "r"(reset_code), "r"((long)reset_len)
+        : "%rax", "%rdi", "%rsi", "%rdx");
 }
--- test.c
+++ test.c
@@ -8,47 +8,14 @@
 
 
 int main() {
-    send_message("hello\n", 9043);
+
     gelb();
-    struct timeval tv;  // The struct to fill with epoch time
-    unsigned char buffer[16];  //8 * 2 = 16 bytes
-    get_time(&tv);
-
-    unsigned char a = 0;
-    for (int i=0;i<16;i++) {
-        for (int j=7;j>=0;j--) {
-            if ((buffer[i] >> j) &1) {
-                printChar('1');
-                if (i == 10) {
-                a += 1<<j;
-                }
-            } else {
-                printChar('0');
-                
-            }
-
-            
-        }
-        printChar('\n');
-        if (i == 7) {printChar('\n');}
-    }
-    printNL();
-
-
-    printChar('h');
-    printChar('\n');
-
-    char string[] = "I really like and love \033[35mWilloh\033[0m ";
-    for (int i=0;i<40;i++) {
-        printChar(string[i]);
-    }
-
-    printChar(a);
-
-    printChar('\n');
     hello();
-    //printNL();
-    
+    print0();
+    print1();
+    printChar('B');
+    newline();
+    gelb;
 
     return 0;
 
